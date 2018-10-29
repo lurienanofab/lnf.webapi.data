cc220962ddda03c4ba13708c61cb0d573943b5de
Jim Getty <jgett@umich.edu>
2018-10-29 10:11:27 -0400

    Added physical access controller.

--- LNF.WebApi.Data/Controllers/UtilityController.cs
+++ LNF.WebApi.Data/Controllers/UtilityController.cs
@@ -13,61 +13,54 @@ namespace LNF.WebApi.Data.Controllers
         [Route("utility/billing-checks/auto-end-problems")]
         public IEnumerable<AutoEndProblem> GetAutoEndProblems(DateTime period)
         {
-            using (var adap = DA.Current.GetAdapter())
+            var dt = DA.Command()
+                .Param("Period", period)
+                .FillDataTable("dbo.BillingChecks_FindAutoEndProblems");
+
+            var result = new List<AutoEndProblem>();
+
+            foreach (DataRow dr in dt.Rows)
             {
-                var dt = adap.AddParameter("@Period", period)
-                    .CommandTypeStoredProcedure()
-                    .FillDataTable("dbo.BillingChecks_FindAutoEndProblems");
-
-
-                var result = new List<AutoEndProblem>();
-
-                foreach(DataRow dr in dt.Rows)
+                var item = new AutoEndProblem
                 {
-                    var item = new AutoEndProblem();
-                    item.ReservationID = dr.Field<int>("ReservationID");
-                    item.AutoEndType = dr.Field<string>("AutoEndType");
-                    item.AutoEndReservation = dr.Field<bool>("AutoEndReservation");
-                    item.AutoEndResource = TimeSpan.FromMinutes(dr.Field<int>("AutoEndResource"));
-                    item.Duration = TimeSpan.FromMinutes(dr.Field<double>("Duration"));
-                    item.EndDateTime = dr.Field<DateTime>("EndDateTime");
-                    item.ActualEndDateTime = dr.Field<DateTime?>("ActualEndDateTime");
-                    item.ActualEndDateTimeExpected = dr.Field<DateTime>("ActualEndDateTimeExpected");
-                    item.Diff = TimeSpan.FromSeconds(dr.Field<int>("DiffSeconds"));
-                    item.ActualEndDateTimeCorrected = dr.Field<DateTime>("ActualEndDateTimeCorrected");
-                    item.ChargeMultiplier = dr.Field<double>("ChargeMultiplier");
-                    result.Add(item);
-                }
+                    ReservationID = dr.Field<int>("ReservationID"),
+                    AutoEndType = dr.Field<string>("AutoEndType"),
+                    AutoEndReservation = dr.Field<bool>("AutoEndReservation"),
+                    AutoEndResource = TimeSpan.FromMinutes(dr.Field<int>("AutoEndResource")),
+                    Duration = TimeSpan.FromMinutes(dr.Field<double>("Duration")),
+                    EndDateTime = dr.Field<DateTime>("EndDateTime"),
+                    ActualEndDateTime = dr.Field<DateTime?>("ActualEndDateTime"),
+                    ActualEndDateTimeExpected = dr.Field<DateTime>("ActualEndDateTimeExpected"),
+                    Diff = TimeSpan.FromSeconds(dr.Field<int>("DiffSeconds")),
+                    ActualEndDateTimeCorrected = dr.Field<DateTime>("ActualEndDateTimeCorrected"),
+                    ChargeMultiplier = dr.Field<double>("ChargeMultiplier")
+                };
 
-                return result;
+                result.Add(item);
             }
+
+            return result;
         }
 
         [HttpGet, Route("utility/billing-checks/auto-end-problems/fix-all")]
         public int FixAllAutoEndProblems(DateTime period)
         {
-            using (var adap = DA.Current.GetAdapter())
-            {
-                var count = adap.CommandTypeStoredProcedure()
-                    .AddParameter("@Period", period)
-                    .ExecuteScalar<int>("dbo.BillingChecks_FixAutoEndProblems");
+            var count = DA.Command()
+                .Param("Period", period)
+                .ExecuteScalar<int>("dbo.BillingChecks_FixAutoEndProblems");
 
-                return count;
-            }
+            return count;
         }
 
         [HttpGet, Route("utility/billing-checks/auto-end-problems/fix")]
         public int FixAutoEndProblem(DateTime period, int reservationId)
         {
-            using (var adap = DA.Current.GetAdapter())
-            {
-                var count = adap.CommandTypeStoredProcedure()
-                    .AddParameter("@Period", period)
-                    .AddParameter("@ReservationID", reservationId)
-                    .ExecuteScalar<int>("dbo.BillingChecks_FixAutoEndProblems");
+            var count = DA.Command()
+                .Param("Period", period)
+                .Param("ReservationID", reservationId)
+                .ExecuteScalar<int>("dbo.BillingChecks_FixAutoEndProblems");
 
-                return count;
-            }
+            return count;
         }
     }
 }
